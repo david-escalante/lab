60c6a9e957e6c8e8ec3240f6e2991c4fe32ba586
David Escalante <david.escalante@rivetlogic.com>
2017-03-13 20:42:13 -0600

    Lab and JS Excercise

---
+++ static-assets/js/practica-1.js
@@ -0,0 +1,14 @@
+/**
+ * Created by davidescalante on 3/13/17.
+ */
+
+window.onload = function() {
+    document.getElementById('discreto').onclick = function () {
+        alert("Llamada");
+    };
+};
+
+
+var alerta = function() {
+    alert("Llamada");
+}
